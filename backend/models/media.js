d79815812d7ce280f0b5a7a6d097a467f561cf35
Vru007 <vrushik7143work@gmail.com>
2025-01-29 12:43:47 +0530

    media, cloudinary, multer

---
+++ backend/models/media.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const mediaSchema = new mongoose.Schema({
+  fileName: { type: String, required: true },
+  fileUrl: { type: String, required: true }, // Cloudinary URL
+  public_id: { type: String, required: true }, // Cloudinary public_id for deletion
+  fileType: { type: String, required: true }, // image/jpeg, video/mp4, etc.
+  uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' }, // Link to User
+  uploadedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Media', mediaSchema);
